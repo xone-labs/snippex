cf552fbe4fc7372b7bed942fea5acb9292c1b6b9
Denish Sharma <denishsharma@ternaengg.ac.in>
2024-02-17 13:31:13 +0530

    fix: replace inter-ui with UnoCSS Web Fonts
    Author: Denish Sharma

--- uno.config.ts
+++ uno.config.ts
@@ -1,12 +1,22 @@
 import ArbitraryVariantsExtractor from "@unocss/extractor-arbitrary-variants";
 import extractorMdc from "@unocss/extractor-mdc";
-import { defineConfig, presetTypography, presetUno, transformerDirectives, transformerVariantGroup } from "unocss";
+import { defineConfig, presetTypography, presetUno, presetWebFonts, transformerDirectives, transformerVariantGroup } from "unocss";
 import { presetExtra } from "unocss-preset-extra";
 import { presetUseful } from "unocss-preset-useful";
 
 export default defineConfig({
     presets: [
         presetUno(),
+        presetWebFonts({
+            provider: "google",
+            fonts: {
+                sans: {
+                    name: "Inter",
+                    weights: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+                    italic: true,
+                },
+            },
+        }),
         presetTypography(),
         presetExtra(),
         presetUseful(),
